fa01b53006b494207450327f4eee080c0ad13287
Alessio9729 <97086582@st.deltion.nl>
2022-02-17 14:05:20 +0100

    fixedxcripts

--- Restless/Assets/Dariel's Scripts/PlayerMovement.cs
+++ Restless/Assets/Dariel's Scripts/PlayerMovement.cs
@@ -13,12 +13,14 @@ public class PlayerMovement : MonoBehaviour
     public bool isSprinting;
     public bool isWalking;
     public bool isCrouching;
+    public Transform cam;
+    public bool crouchToggle;
 
     void Start()
     {
-        crouchSpeed = 3f;
+        crouchSpeed = 2f;
         walkSpeed = 5f;
-        sprintSpeed = 8f;
+        sprintSpeed = 12f;
     }
 
     void Update()
@@ -29,28 +31,61 @@ public class PlayerMovement : MonoBehaviour
         movement.z = vertical;
         transform.Translate(movement * walkSpeed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             isSprinting = true;
             isWalking = false;
-            walkSpeed = sprintSpeed;
+            walkSpeed = 12f;
         }
-        else
+        else if(Input.GetKeyUp(KeyCode.LeftShift))
         {
             isSprinting = false;
             isWalking = true;
             walkSpeed = 5f;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKey(KeyCode.LeftControl))
         {
             isCrouching = true;
             walkSpeed = crouchSpeed;
         }
-        else if (Input.GetKeyDown(KeyCode.LeftControl))
+        else
         {
             isCrouching = false;
-            walkSpeed = 5f;
+            if (isCrouching == false)
+            {
+              if(isSprinting == false)
+              {
+                walkSpeed = 5f;
+              }
+            }
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (crouchToggle == false)
+            {
+                crouchToggle = true;
+                transform.localScale -= new Vector3(0f, 0.60f, 0f);
+                transform.localPosition -= new Vector3(0f, 0.30f, 0f);
+
+               
+
+
+            }
+        
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            crouchToggle = false;
+            transform.localScale += new Vector3(0f, 0.60f, 0f);
+            transform.localPosition += new Vector3(0f, 0.30f, 0f);
+        }
+
+
     }
+
+    
+  
+    
 }
