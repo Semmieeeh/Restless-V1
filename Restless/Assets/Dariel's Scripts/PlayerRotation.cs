fee5521fabbcab38f6fc03487b698bce97ddd73e
Dariel2004 <99257788+Dariel2004@users.noreply.github.com>
2022-03-11 11:45:39 +0100

    yes

--- Restless/Assets/Dariel's Scripts/PlayerRotation.cs
+++ Restless/Assets/Dariel's Scripts/PlayerRotation.cs
@@ -14,7 +14,7 @@ public class PlayerRotation : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
