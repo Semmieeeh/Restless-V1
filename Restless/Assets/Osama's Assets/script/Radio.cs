6a7af9399ab76d7e7d960e5f8ae06c3f95f99991
gastenzijncool <99257768+gastenzijncool@users.noreply.github.com>
2022-03-12 22:52:30 +0100

    schilderij

--- Restless/Assets/Osama's Assets/script/Radio.cs
+++ Restless/Assets/Osama's Assets/script/Radio.cs
@@ -6,6 +6,7 @@ public class Radio : MonoBehaviour
 {
     public RaycastHit hit;
     public GameObject sM;
+    public GameObject fFM;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +16,25 @@ public class Radio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 3))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 5))
         {
             if (hit.transform.gameObject.tag == "radio")
             {
                 if (Input.GetKeyDown("e"))
                 {
                     sM.SetActive(false);
+                    fFM.SetActive(false);
+                }
+
+            }
+        }
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 5))
+        {
+            if (hit.transform.gameObject.tag == "FF7")
+            {
+                if (Input.GetKeyDown("e"))
+                {
+                    fFM.SetActive(true);
                 }
 
             }
